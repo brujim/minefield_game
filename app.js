0e72975aef162bc6ec3847f36e124acb7038508e
BRUNO POSTALI <bruno.postali@gmail.com>
2022-03-16 18:16:41 -0300

    Rand mines adjusted / New Styles

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function field(cols_count, rows_count, mines){
     var rows = [];
 
-    //Put mines in field
+    //Put mines and 0 in field
     for (var i = 0; i < rows_count; i++){
         rows[i] = [];
         for (var j = 0; j < cols_count; j++){
@@ -26,30 +26,16 @@ function field(cols_count, rows_count, mines){
 
                 if (rows[i + 1] !== undefined && rows[i + 1][j - 1] === "*") rows[i][j]++;
                 if (rows[i + 1] !== undefined && rows[i + 1][j    ] === "*") rows[i][j]++;
-                if (rows[i + 1] !== undefined && rows[i + 1][j + 1] === "*") rows[i][j]++;
+                if (rows[i + 1] !== undefined && rows[i + 1][j + 1] === "*") rows[i][j]++;               
             }
         }
     }
 
     return rows;
 }
-//New game reload
-function reload() {
-   return document.location.reload(true);  
-}
-//Game execution
-function gameHandle(event) {
-    if(event.target.textContent === '*'){
-        for (element of document.querySelectorAll('span')){
-            element.setAttribute('class', '')
-        }
-        alert('Você perdeu!');
-    } else {
-        event.target.childNodes[0].setAttribute('class', '');
-    }
-}
 
 function drawTable(rows) {
+
     var table = document.getElementById('field');
     for (var row of rows) {
         var tr = document.createElement('tr');
@@ -65,16 +51,55 @@ function drawTable(rows) {
         table.appendChild(tr);
     }
 }
-//Randomize mines **need fix when get existent array to push a new mine 
+
+//Randomize mines
 function randomMines (quantity, rows, cols) {
     var mine = [];
-    for (var i = 0; i < quantity; i++){
+    while(mine.length < quantity){
         var posRow = parseInt(Math.random() * rows);
-        var posCol = parseInt(Math.random() * cols);
+        var posCol = parseInt(Math.random() * cols); 
+        if(mine.filter(item => { return item[0] === posRow && item[1] === posCol;}).length > 0){
+            continue;
+        }    
         mine.push([posRow, posCol]);
+        mine.sort();
     }
-    return mine;
+    return mine;   
 }
 
-var mines = randomMines(10, 8, 8) // Number of Mines / Rows / Cols
-drawTable(field(8, 8, mines)); // Rows / Cows / Mines
+
+function gameHandle(event) {
+    console.log(event.target.textContent)
+    if(event.target.textContent === '*'){
+        for (var element of document.querySelectorAll('span')){
+            element.setAttribute('class', 'red')
+            if (element.textContent === '*'){
+                element.textContent = '';
+                var createFlag = document.createElement('img');
+                createFlag.setAttribute('src', 'mine.png')
+                element.appendChild(createFlag)
+            } 
+        }
+        alert('Você perdeu!');
+    } else {
+        event.target.childNodes[0].setAttribute('class', '');
+        if (event.target.childNodes[0].textContent === '1'){
+            event.target.childNodes[0].setAttribute('class', 'blue')
+        } else if (event.target.childNodes[0].textContent === '2'){
+            event.target.childNodes[0].setAttribute('class', 'red')
+        } else if (event.target.childNodes[0].textContent === '3'){
+            event.target.childNodes[0].setAttribute('class', 'green')
+        } else if (event.target.childNodes[0].textContent === '4'){
+            event.target.childNodes[0].setAttribute('class', 'purple')
+        }   
+    }
+}
+
+//New game reload
+function reload() {
+    return document.location.reload(true);  
+ }
+
+var mines = randomMines(8, 10, 10) // Number of Mines / Rows / Cols
+drawTable(field(10, 10, mines)); // Rows / Cows / Mines
+
